b7a760a787896ab46c98a35350b0a4a1a50f3822
Archit Kansal <archit.knsl@gmail.com>
2016-11-05 14:01:28 +0530

    popup salt added

---
+++ MediSurf/app/src/main/java/com/medisurf/medisurf/Display_Salt.java
@@ -0,0 +1,36 @@
+package com.medisurf.medisurf;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Display_Salt extends AppCompatActivity {
+    String salt;
+    String desc;
+    Bundle extras;
+    EditText salt_name;
+    EditText Desc;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display__salt);
+
+        DisplayMetrics dm=new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int width=dm.widthPixels;
+        int height=dm.heightPixels;
+
+        getWindow().setLayout((int) (width * 0.8), (int) (height * 0.35));
+        extras = getIntent().getExtras();
+        salt = extras.getString("salt");
+        desc = extras.getString("desc");
+        salt_name  = (EditText) findViewById(R.id.salt_name);
+        Desc = (EditText) findViewById(R.id.salt_description);
+        salt_name.setText(salt.toString());
+        Desc.setText(desc.toString());
+    }
+
+}
--- MediSurf/app/src/main/java/com/medisurf/medisurf/getSalt.java
+++ MediSurf/app/src/main/java/com/medisurf/medisurf/getSalt.java
@@ -18,6 +18,8 @@ import org.json.JSONObject;
 
 import java.util.HashMap;
 
+import static android.R.attr.name;
+
 public class getSalt extends AppCompatActivity implements AsyncResponse{
 
     EditText med_name;
@@ -81,13 +83,21 @@ public class getSalt extends AppCompatActivity implements AsyncResponse{
         try {
             if (jObj.getString("success").equals("1"))
             {
+
                 Toast.makeText(this, "Account created Successfully",
                         Toast.LENGTH_LONG).show();
+
+                Intent i = new Intent(this, Display_Salt.class);
+                i.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                i.putExtra("salt", jObj.getString("generic_salt"));
+                i.putExtra("desc" , jObj.getString("description"));
+                startActivity(i);
                 this.finish();
             }
             else
             {
-                Toast.makeText(this, "Login Failed!!!",
+                Toast.makeText(this, "Connection Failed!",
                         Toast.LENGTH_LONG).show();
             }
         }
