6fd17a46789835ebea3ada58a1e066bf10db8a84
Vinay Gedam <gedamvinay@gmail.com>
2017-01-29 16:53:08 +0530

    Commits made #din ki shuruvat

--- medisurf_python/medisurf/urls.py
+++ medisurf_python/medisurf/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     url(r'^save_me/',save_money),
     url(r'^region_usage/',region_usage),
     url(r'^usage_stat',usage_stats),
+    url(r'^purchase_stats',purchase_stats),
     url(r'^foo',foo),
     url(r'^select_state',select_state),
     url(r'^select_dis',select_dis),
--- medisurf_python/medisurf/views.py
+++ medisurf_python/medisurf/views.py
@@ -4,7 +4,7 @@ import json
 from django.http import HttpResponse, HttpResponseRedirect,JsonResponse
 # Create your views here.
 
-
+#main landing page
 def home(request):
 	# for i in Medicine.objects.raw('SELECT * from medicine'):
 	# 	print i.name
@@ -45,6 +45,7 @@ def region_usage(request):
 		print u.latitude + " " +u.longitude + " "+u.alternative
 	return HttpResponse('')
 
+#function for number of users of a generic salt
 def usage_stats(request):
 	query_string = "SELECT alternatives.*,medicine.generic_salt from alternatives,medicine where "
 	query_param = []
@@ -91,9 +92,63 @@ def usage_stats(request):
 		print total
 		for q in query:
 			try:
-			   qdata[q.generic_salt] +=1;
+			   qdata[q.generic_salt] +=1
 			except KeyError:
-			   qdata[q.generic_salt] =1;
+			   qdata[q.generic_salt] =1
+		response['qdata']=qdata
+		response['total']=total
+	return HttpResponse(json.dumps(response),content_type = "application/json")
+
+#function for purchases of a generic salt
+def purchase_stats(request):
+	query_string = "SELECT alternatives.*,medicine.generic_salt,medicine.price from alternatives,medicine where "
+	query_param = []
+	if(request.POST):
+		if(request.POST['age_grp'] != 'all'):
+			age = request.POST['age_grp']
+			words = age.split("-")
+			low = words[0]
+			high = words[1]
+			print low
+			query_string+="age_grp>= %s and age_grp<= %s and "
+			query_param.append(low)
+			query_param.append(high)
+		if(request.POST['sex'] != 'all'):
+			sex = request.POST['sex']
+			query_string+="sex = %s and "
+			query_param.append(sex)
+		if(request.POST['month'] != 'all'):
+			month = request.POST['month']
+			# query_string+="month = %s and "
+			# query_param.append(month)
+		if(request.POST['Suburb'] != 'all'):
+			suburb = request.POST['Suburb']
+			query_string+="suburb = %s and "
+			query_param.append(suburb)
+		if(request.POST['District'] != 'all'):
+			district = request.POST['District']
+			query_string+="district = %s and "
+			query_param.append(district)
+		if(request.POST['State'] != 'all'):
+			state = request.POST['State']
+			query_string+="state = %s and "
+			query_param.append(state)
+		if(request.POST['Country'] != 'all'):
+			country = request.POST['Country']
+			query_string+="country = %s and "
+			query_param.append(country)
+		query_string += "alternatives.alternative = medicine.name"
+		print query_string
+		query = Alternatives.objects.raw(query_string,query_param)
+		response = {}
+		qdata={}
+		total = len(list(query))
+		print total
+		for q in query:
+			try:
+			   qdata[q.generic_salt] +=q.price
+			except KeyError:
+			   qdata[q.generic_salt] =q.price
 		response['qdata']=qdata
 		response['total']=total
 	return HttpResponse(json.dumps(response),content_type = "application/json")
