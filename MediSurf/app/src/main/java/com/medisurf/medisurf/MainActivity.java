9d3d6a6567f8c3b112657ea3348bcb048b8b4d0c
anshul-goyal <anshul.goyal7@gmail.com>
2017-08-25 01:12:42 +0530

    UI changes

--- MediSurf/app/src/main/java/com/medisurf/medisurf/MainActivity.java
+++ MediSurf/app/src/main/java/com/medisurf/medisurf/MainActivity.java
@@ -2,6 +2,8 @@ package com.medisurf.medisurf;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.NetworkRequest;
@@ -15,6 +17,7 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,6 +29,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.HashMap;
+
+import jp.wasabeef.blurry.Blurry;
 
 import static com.medisurf.medisurf.URLGenerator.ip;
 
@@ -50,8 +55,16 @@ public class MainActivity extends AppCompatActivity {
         Drawer drawer= (Drawer) getSupportFragmentManager().findFragmentById(R.id.drawer_fragment);
         drawer.setup(R.id.drawer_fragment, (DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
 
+//        Blurry.with(MainActivity.this)
+//                .radius(25)
+//                .sampling(1)
+//                .color(Color.argb(66, 0, 255, 255))
+//                .async()
+//                .capture(findViewById(R.id.my_pic))
+//                .into((ImageView) findViewById(R.id.my_pic));
+
         counts = (TextView) (findViewById(R.id.text_number));
-        counts.setText("11523");
+        counts.setText("₹11523");
         LinearLayout l4 = (LinearLayout) findViewById(R.id.l4);
         Display display = ((WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
         int width = display.getWidth() / 2;
@@ -62,44 +75,17 @@ public class MainActivity extends AppCompatActivity {
         TextView tv2 = new TextView(this);
         tv1.setText("PRESCRIBED");
         tv2.setText("ALTERNATIVE");
+        tv1.setTextColor(Color.parseColor("#01579B"));
+        tv2.setTextColor(Color.parseColor("#01579B"));
+        tv1.setTypeface(null, Typeface.BOLD);
+        tv2.setTypeface(null, Typeface.BOLD);
         tv1.setGravity(Gravity.CENTER);
         tv2.setGravity(Gravity.CENTER);
-        tv1.setTextSize(18);
-        tv2.setTextSize(18);
+        tv1.setTextSize(25);
+        tv2.setTextSize(25);
         l.addView(tv1,lp);
         l.addView(tv2, lp);
         l4.addView(l);
-//        if(isNetworkAvailable()) {
-//
-//            HashMap postData = new HashMap();
-//            postData.put("btnLogin", "Login");
-//            postData.put("mobile", "android");
-//            PostResponseAsyncTask loginTask =
-//                    new PostResponseAsyncTask(MainActivity.this, postData);
-//            System.out.println("Before Logging in");
-//            loginTask.execute("http://" + ip + "/get_count.php");
-//            System.out.println("After Logging in....");
-//
-//            HashMap postData2 = new HashMap();
-//            postData.put("btnLogin", "Login");
-//            postData.put("mobile", "android");
-//            PostResponseAsyncTask loginTask2 =
-//                    new PostResponseAsyncTask(MainActivity.this, postData);
-//            System.out.println("Before Logging in");
-//            loginTask2.execute("http://" + ip + "/most_frequent.php");
-//            System.out.println("After Logging in....");
-//
-//        }
-//        else
-//        {
-//            Context context = getApplicationContext();
-//            CharSequence text = "No network found. Try again later!";
-//            int duration = Toast.LENGTH_SHORT;
-//
-//            Toast toast = Toast.makeText(context, text, duration);
-//            toast.show();
-//        }
-
     }
 
     public void processFinish(JSONObject jObj) {
@@ -107,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
         try {
             if (jObj.getString("success").equals("1"))
             {
-                counts.setText(jObj.getString("count").toString());
+                counts.setText("₹"+jObj.getString("count").toString());
                 JSONArray jarr = jObj.getJSONArray("results");
 
                 for(int i=0;i<jarr.length();i++)
@@ -123,12 +109,16 @@ public class MainActivity extends AppCompatActivity {
                     LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(width, LinearLayout.LayoutParams.WRAP_CONTENT);
                     TextView tv1 = new TextView(this);
                     TextView tv2 = new TextView(this);
+                    tv1.setTextColor(Color.parseColor("#01579B"));
+                    tv2.setTextColor(Color.parseColor("#01579B"));
+                    tv1.setTypeface(null, Typeface.BOLD);
+                    tv2.setTypeface(null, Typeface.BOLD);
                     tv1.setText(orig);
                     tv2.setText(alter);
                     tv2.setGravity(Gravity.CENTER);
                     tv1.setGravity(Gravity.CENTER);
-                    tv2.setTextSize(15);
-                    tv1.setTextSize(15);
+                    tv2.setTextSize(20);
+                    tv1.setTextSize(20);
                     l.addView(tv1,lp);
                     l.addView(tv2, lp);
                     l4.addView(l);
